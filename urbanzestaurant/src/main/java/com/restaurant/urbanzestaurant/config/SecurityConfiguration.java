ec79846a355d1acca84e64f3444267ec4d15b086
Pavangalagali <59006870+Pavangalagali@users.noreply.github.com>
2025-08-27 13:18:51 +0530

    Update SecurityConfiguration.java
    Origins

--- urbanzestaurant/src/main/java/com/restaurant/urbanzestaurant/config/SecurityConfiguration.java
+++ urbanzestaurant/src/main/java/com/restaurant/urbanzestaurant/config/SecurityConfiguration.java
@@ -95,7 +95,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOriginPatterns(List.of("http://localhost:3000","http://localhost:3001"));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
